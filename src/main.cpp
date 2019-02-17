f632313ac0a6f09ac5d8238ede1269d1cfb0f97a
KTStephano <ktstephano@gmail.com>
2019-02-16 23:08:45 -0700

    Adding support for OpenGL with SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "GL/gl3w.h"
+#include "GL/gl.h"
 #include "SDL2/SDL.h"
 #include <iostream>
 
@@ -9,18 +10,25 @@ int main(int argc, char * args[]) {
         return -1;
     }
 
-    SDL_Window * window = SDL_CreateWindow("StratusGFX", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window * window = SDL_CreateWindow("StratusGFX",
+            100, 100, // location x/y on screen
+            640, 480, // width/height of window
+            SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL );
     if (window == nullptr) {
         std::cout << "Failed to create sdl window" << std::endl;
         SDL_Quit();
         return -1;
     }
 
-    // Note that -1 tells it to pick the first rendering driver that meets
-    // out requirements
-    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == nullptr) {
-        std::cout << "Unable to create hardware accelerated renderer" << std::endl;
+    SDL_GL_SetAttribute(SDL_GLattr::SDL_GL_CONTEXT_PROFILE_MASK,
+        SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (context == nullptr) {
+        std::cout << "Unable to create a valid OpenGL context" << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
         return -1;
@@ -49,11 +57,13 @@ int main(int argc, char * args[]) {
             }
         }
 
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Swap front and back buffer
+        SDL_GL_SwapWindow(window);
     }
 
-    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
