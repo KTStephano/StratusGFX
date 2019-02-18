c66a9513614909babc4c1b3bfbfd42cf5c219281
KTStephano <ktstephano@gmail.com>
2019-02-18 01:19:24 -0700

    Adds position, scale and rotation to RenderEntity

--- includes/RenderEntity.h
+++ includes/RenderEntity.h
@@ -55,6 +55,10 @@ class RenderEntity {
     RenderMaterial _material;
 
 public:
+    glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f);
+    glm::mat4 rotation = glm::mat4(1.0f);
+
     /**
      * @param mode determines whether 2d/3d is necessary
      * @param properties render properties which decides which
