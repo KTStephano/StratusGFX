c1e35cdd5cf7b655117effbbc1a017c5b24cace7
Justin Hall <ktstephano@gmail.com>
2021-09-19 23:52:09 -0600

    Adds fix for most peter panning; reduces max distance of shadow maps to 700

--- resources/shaders/pbr.fs
+++ resources/shaders/pbr.fs
@@ -203,9 +203,9 @@ float sampleShadowTexture(sampler2DArrayShadow shadow, vec4 coords, float depth,
 float calculateInfiniteShadowValue(vec4 fragPos, vec3 cascadeBlends, vec3 normal) {
 	// Since dot(l, n) = cos(theta) when both are normalized, below should compute tan theta
     // See: http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-16-shadow-mapping/
-	float tanTheta = tan(acos(dot(normalize(infiniteLightDirection), normal)));
+	float tanTheta = 3.0 * tan(acos(dot(normalize(infiniteLightDirection), normal)));
     float bias = 0.005 * tanTheta;
-    bias = clamp(bias, 0.0, 0.01);
+    bias = clamp(bias, 0.0, 0.002);
 
     vec4 p1, p2;
     vec3 cascadeCoords[4];
