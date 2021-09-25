e0472c5f90363d80e03c5973b457b03670b4d4ec
Justin Hall <ktstephano@gmail.com>
2021-09-25 06:55:52 -0600

    Update shaders

--- resources/shaders/bloom.fs
+++ resources/shaders/bloom.fs
@@ -11,7 +11,7 @@ uniform bool finalStage        = false;
 uniform sampler2D mainTexture;
 uniform sampler2D bloomTexture;
 
-uniform float bloomThreshold = 1.25;
+uniform float bloomThreshold = 1.0;
 uniform float upsampleRadiusScale = 0.5;
 
 uniform float viewportX;
