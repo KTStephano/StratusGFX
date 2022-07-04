4ee7e873dc7a5447d3be1ce00b5b8a551e42da11
Justin Hall <ktstephano@gmail.com>
2022-07-04 01:20:35 -0600

    Adding more replace tests

--- Tests/TestUtils.cpp
+++ Tests/TestUtils.cpp
@@ -8,4 +8,16 @@ TEST_CASE( "Testing replace", "[replace_test]" ) {
     std::string source = "";
     REQUIRE(stratus::ReplaceFirst(source, "1", "one") == false);
     REQUIRE(source == "");
+
+    source = "1";
+    REQUIRE(stratus::ReplaceFirst(source, "1", "one") == true);
+    REQUIRE(source == "one");
+
+    source = "1 2 3 4 1 5 6 1 7 8";
+    REQUIRE(stratus::ReplaceFirst(source, "1", "one") == true);
+    REQUIRE(source == "one 2 3 4 1 5 6 1 7 8");
+
+    source = "1 2 3 4 1 5 6 1 7 8";
+    REQUIRE(stratus::ReplaceAll(source, "1", "one") == true);
+    REQUIRE(source == "one 2 3 4 one 5 6 one 7 8");
 }
