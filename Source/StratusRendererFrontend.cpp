61db31fa9f9d6ca0d7b3e89ccdaa3e679214249f
Justin Hall <ktstephano@gmail.com>
2023-02-18 05:01:06 -0700

    Switching to 2k x 2k cascade textures to improve performance

--- Source/StratusRendererFrontend.cpp
+++ Source/StratusRendererFrontend.cpp
@@ -411,7 +411,7 @@ namespace stratus {
 
         // 4 cascades total
         _frame->csc.cascades.resize(4);
-        _frame->csc.cascadeResolutionXY = 4096;
+        _frame->csc.cascadeResolutionXY = 2048;
         _frame->csc.regenerateFbo = true;
 
         // Set materials per frame and initialize material buffer
