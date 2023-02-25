3dac709dc25e9f36220ad024a7573994e086d95a
Justin Hall <ktstephano@gmail.com>
2023-02-25 08:29:00 -0700

    Adds atomic add float

--- Source/Shaders/vpl_tiled_deferred_culling.glsl
+++ Source/Shaders/vpl_tiled_deferred_culling.glsl
@@ -4,3 +4,29 @@ STRATUS_GLSL_VERSION
 // TODO: Find a better way to sync these values with renderer
 #define MAX_TOTAL_VPLS_PER_FRAME (300)
 #define MAX_VPLS_PER_TILE (5)
+
+// Needs to use uint for its memory backing since GLSL atomics only work on int and uint
+// See for reference:
+//  https://codereview.stackexchange.com/questions/258883/glsl-atomic-float-add-for-architectures-with-out-atomic-float-add
+//  https://registry.khronos.org/OpenGL-Refpages/gl4/html/atomicCompSwap.xhtml
+//  https://registry.khronos.org/OpenGL-Refpages/gl4/html/atomicAdd.xhtml
+//  https://registry.khronos.org/OpenGL-Refpages/gl4/html/floatBitsToInt.xhtml
+//
+// Returns the original contents of mem before the add
+//
+// See https://community.khronos.org/t/atomiccompswap/69213 for why it needs to be a #define
+//
+// Type of mem: uint
+// Type of data: float
+// Type of old: uint
+#define ATOMIC_ADD_FLOAT(mem, data, old) {                              \
+    uint expected = mem;                                                \
+    uint added    = floatBitsToUint(uintBitsToFloat(mem) + data);       \
+    uint returned = atomicCompSwap(mem, expected, added);               \
+    while (expected != returned) {                                      \
+        expected = returned;                                            \
+        added    = floatBitsToUint(uintBitsToFloat(expected) + data);   \
+        returned = atomicCompSwap(mem, expected, added);                \
+    }                                                                   \
+    old = returned;                                                     \
+}
