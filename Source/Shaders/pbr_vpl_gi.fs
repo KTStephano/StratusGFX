1b008f40f9998b62eeffc1b9f5b21e7279e49bce
Justin Hall <ktstephano@gmail.com>
2023-02-28 07:37:31 -0700

    Slight performance improvement by calculating low quality shadows (or none at all) based on fragment distance to camera

--- Source/Shaders/pbr_vpl_gi.fs
+++ Source/Shaders/pbr_vpl_gi.fs
@@ -66,6 +66,7 @@ vec3 performLightingCalculations(vec3 screenColor, vec2 pixelCoords, vec2 texCoo
 
     vec3 fragPos = texture(gPosition, texCoords).rgb;
     vec3 viewDir = normalize(viewPosition - fragPos);
+    float distToCamera = length(viewPosition - fragPos);
 
     vec3 baseColor = texture(gAlbedo, texCoords).rgb;
     vec3 normal = normalize(texture(gNormal, texCoords).rgb * 2.0 - vec3(1.0));
@@ -91,12 +92,12 @@ vec3 performLightingCalculations(vec3 screenColor, vec2 pixelCoords, vec2 texCoo
         //if (distance > lightRadii[lightIndex]) continue;
 
         float shadowFactor = 0.0;
-        // if (ratio < 0.25) {
-        shadowFactor = calculateShadowValue8Samples(shadowCubeMaps[lightIndex], lightData[lightIndex].farPlane, fragPos, lightPosition, dot(lightPosition - fragPos, normal));
-        // }
-        //else if (ratio < 0.25) {
-        //    shadowFactor = calculateShadowValue1Sample(shadowCubeMaps[lightIndex], lightFarPlanes[lightIndex], fragPos, lightPosition, dot(lightPosition - fragPos, normal));
-        //}
+        if (distToCamera < 200) {
+            shadowFactor = calculateShadowValue8Samples(shadowCubeMaps[lightIndex], lightData[lightIndex].farPlane, fragPos, lightPosition, dot(lightPosition - fragPos, normal));
+        }
+        else if (distToCamera < 400) {
+            shadowFactor = calculateShadowValue1Sample(shadowCubeMaps[lightIndex], lightData[lightIndex].farPlane, fragPos, lightPosition, dot(lightPosition - fragPos, normal));
+        }
         // Depending on how visible this VPL is to the infinite light, we want to constrain how bright it's allowed to be
         //shadowFactor = lerp(shadowFactor, 0.0, vpl.shadowFactor);
 
