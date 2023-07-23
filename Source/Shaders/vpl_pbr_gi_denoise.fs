58e7481dd97e735620c9c261c427b3d4dc8546d1
KTStephano <ktstephano@gmail.com>
2023-07-22 22:33:46 -0600

    Removes most of the commented out code in the denoiser shader

--- Source/Shaders/vpl_pbr_gi_denoise.fs
+++ Source/Shaders/vpl_pbr_gi_denoise.fs
@@ -289,12 +289,6 @@ void main() {
     float historyAccum = texture(historyDepth, fsTexCoords).r;
     //float prevCenterDepth = texture(depth, prevTexCoords).r;
 
-    // vec3 centerShadow = texture(indirectShadows, fsTexCoords).rgb;
-    // vec3 topShadow    = textureOffset(indirectShadows, fsTexCoords, ivec2( 0,  1)).rgb;
-    // vec3 botShadow    = textureOffset(indirectShadows, fsTexCoords, ivec2( 0, -1)).rgb;
-    // vec3 rightShadow  = textureOffset(indirectShadows, fsTexCoords, ivec2( 1,  0)).rgb;
-    // vec3 leftShadow   = textureOffset(indirectShadows, fsTexCoords, ivec2(-1,  0)).rgb;
-
     vec4 reservoirFiltered = vec4(0.0);
     //vec3 shadowFactor = vec3(0.0);
     //float numShadowSamples = 0.0;
@@ -327,50 +321,7 @@ void main() {
     //vec3 shadowFactor = reservoirFiltered.rgb / max(PREVENT_DIV_BY_ZERO, numShadowSamples + reservoirFiltered.a);
     vec3 shadowFactor = reservoirFiltered.rgb / max(PREVENT_DIV_BY_ZERO, reservoirFiltered.a);
 
-    //vec3 minColor = tonemap(min(centerIllum, min(topIllum, min(botIllum, min(rightIllum, leftIllum)))));
-    //vec3 maxColor = tonemap(max(centerIllum, max(topIllum, max(botIllum, max(rightIllum, leftIllum)))));
-
-    //vec3 gi = (centerIllum + topIllum + botIllum + rightIllum + leftIllum) / 5.0;
     vec3 gi = centerIllum;
-    // if (final) {
-    //     vec3 topIllum    = textureOffset(indirectIllumination, fsTexCoords, ivec2( 0,  1)).rgb;
-    //     vec3 botIllum    = textureOffset(indirectIllumination, fsTexCoords, ivec2( 0, -1)).rgb;
-    //     vec3 rightIllum  = textureOffset(indirectIllumination, fsTexCoords, ivec2( 1,  0)).rgb;
-    //     vec3 leftIllum   = textureOffset(indirectIllumination, fsTexCoords, ivec2(-1,  0)).rgb;
-    //     gi = (centerIllum + topIllum + botIllum + rightIllum + leftIllum) / 5.0;
-    // }
-    //gi = gi * shadowFactor;// * variance;
-    //vec3 shadow = shadowFactor;
-    //vec3 shadow = centerShadow;
-    //gi = gi;
-    // vec3 gi = centerIllum / (baseColor + PREVENT_DIV_BY_ZERO);
-    //vec3 gi = centerIllum;
-
-    // vec3 gi = vec3(0.0);
-    // float numGiSamples = 0.0;
-    // for (int dx = -1; dx <= 1; ++dx) {
-    //     for (int dy = -1; dy <= 1; ++dy) {
-    //         ++numGiSamples;
-    //         //vec3 currAlbedo = textureOffset(albedo, fsTexCoords, ivec2(dx, dy) + ivec2(dx, dy) * multiplier).rgb;
-    //         vec3 illum = textureOffset(indirectIllumination, fsTexCoords, ivec2(dx, dy) + ivec2(dx, dy) * multiplier).rgb;
-    //         // currAlbedo = vec3(
-    //         //     max(currAlbedo.r, COMPONENT_WISE_MIN_VALUE), 
-    //         //     max(currAlbedo.g, COMPONENT_WISE_MIN_VALUE), 
-    //         //     max(currAlbedo.b, COMPONENT_WISE_MIN_VALUE)
-    //         // );
-    //         gi += illum;
-    //         //gi += vec3(luminance);
-    //     }
-    // }
-    // gi /= numGiSamples;
-
-    // vec3 tmPrevGi = tonemap(prevGi);
-    // tmPrevGi = vec3(
-    //     clamp(tmPrevGi.r, minColor.r, maxColor.r),
-    //     clamp(tmPrevGi.g, minColor.g, maxColor.g),
-    //     clamp(tmPrevGi.b, minColor.b, maxColor.b)
-    // );
-    // prevGi = inverseTonemap(tmPrevGi);
 
     //vec3 illumAvg = gi * shadowFactor;
     vec3 illumAvg = gi;
@@ -385,55 +336,6 @@ void main() {
 
         //vec3 currGi = gi * shadowFactor;
         vec3 currGi = shadowFactor;
-        //float currLum = linearColorToLuminance(tonemap(currGi));
-        //float variance = calculateLuminanceVariance(fsTexCoords, 0);
-
-        // vec3 currColor1 = textureOffset(screen, fsTexCoords, ivec2( 0,  1)).rgb;
-        // vec3 currColor2 = textureOffset(screen, fsTexCoords, ivec2( 0, -1)).rgb;
-        // vec3 currColor3 = textureOffset(screen, fsTexCoords, ivec2( 1,  0)).rgb;
-        // vec3 currColor4 = textureOffset(screen, fsTexCoords, ivec2(-1,  0)).rgb;
-
-        // vec3 minColor = tonemap(min(currentColor, min(currColor1, min(currColor2, min(currColor3, currColor4)))));
-        // vec3 maxColor = tonemap(max(currentColor, max(currColor1, max(currColor2, max(currColor3, currColor4)))));
-
-        // Looks in a 3x3 temporal neighborhood to check the current sample against the geometry and lighting values
-        // of the temporal neighborhood
-        // for (int dx = -1; dx <= 1 && !complete; dx += 2) {
-        //     for (int dy = -1; dy <= 1 && !complete; dy += 2) {
-        //         ++totalSamples;
-
-        //         float prevCenterDepth = textureOffset(depth, prevTexCoords, ivec2(dx, dy)).r;
-        //         prevCenterNormal = sampleNormalWithOffset(prevNormal, prevTexCoords, ivec2(dx, dy));
-        //         vec3 prevGi = textureOffset(prevIndirectIllumination, prevTexCoords, ivec2(dx, dy)).rgb;
-
-        //         float wn = max(0.0, dot(centerNormal, prevCenterNormal));
-        //         wn = pow(wn, 8.0);
-        //         //if (wn < 0.95) wn = 0.0;
-                
-        //         //float wz = exp(-abs(centerDepth - prevCenterDepth) / (sigmaZ * abs(dot(currGradient, fsTexCoords - prevTexCoords)) + 0.0001));
-        //         float wz = exp(-50.0 * abs(centerDepth - prevCenterDepth));
-        //         //float wz = abs(centerDepth = prevCenterDepth);
-        //         //float wz = 1.0 - abs(centerDepth - prevCenterDepth);
-        //         //if (wz < 0.96) wz = 0.0;
-        //         //wz = 0.0;
-
-        //         //float wrt = length(prevGi - currGi);
-        //         float wrt = abs(linearColorToLuminance(tonemap(prevGi)) - currLum);
-        //         float ozrt = 5.0;//4 * exp(-variance) + 0.0001;
-        //         //ozrt = 1.0 - variance + 0.0001;
-        //         wrt = exp(-wrt / ozrt);
-        //         //if (wrt < 0.97) wrt = 0.0;
-
-        //         float similarity = 1 * 1 * 1;
-                
-        //         if (similarity > 0.95) {
-        //             ++similarSamples;
-        //             similarity = 0.0;
-        //             //accumMultiplier = 0.0;
-        //             //complete = true;
-        //         }
-        //     }
-        // }
 
         // float similarity = similarSamples / totalSamples;
         // if (similarity < 0.25) {
@@ -498,134 +400,13 @@ void main() {
 
         historyAccum = min(1.0 + historyAccum * accumMultiplier, framesPerSecond);
 
-        //shadowFactor = max(shadowFactor, 0.0025);
-        //illumAvg = mix(prevGi, gi * shadowFactor, 0.05);
-        //illumAvg = mix(prevGi, gi * shadowFactor, 0.1);
         float maxAccumulationFactor = 1.0 / historyAccum;
         illumAvg = mix(prevGi, currGi, maxAccumulationFactor);
         //illumAvg = shadowFactor;
-        //illumAvg = mix(prevGi, currGi, maxAccumulationFactor / max(maxAccumulationFactor, similarity));
-        //illumAvg = gi * shadowFactor;
-        //illumAvg = vec3(wz);
-        //illumAvg = vec3(similarity);
-        //illumAvg = vec3(variance);
-        //illumAvg = shadowFactor;
     }
-    //vec3 illumAvg = shadowFactor;
-    //vec3 illumAvg = vec3(variance);
-    // a is effectively how many frames we can accumulate. So for example 1 / 0.1 = 10,
-    // 1 / 0.05 = 20, etc.
-    //vec3 illumAvg = mix(prevGi, gi, 0.05);
-    //vec3 illumAvg = mix(prevGi, shadowFactor, 0.05);
-    //vec3 illumAvg = mix(prevGi, gi, 1.0 / 1000.0);
-    //vec3 illumAvg = vec3(difference);
 
     combinedColor = screenColor + gi * illumAvg;
     giColor = illumAvg;
     reservoirValue = vec4(shadowFactor, 1.0);
     newHistoryDepth = historyAccum;
 }
-
-// void main() {
-//     vec3 screenColor = texture(screen, fsTexCoords).rgb;
-//     vec2 velocityVal = texture(velocity, fsTexCoords).xy;
-//     vec2 prevTexCoords = fsTexCoords - velocityVal;
-//     //vec3 baseColor = texture(albedo, fsTexCoords).rgb;
-
-//     vec3 centerIllum = texture(indirectIllumination, fsTexCoords).rgb;
-//     vec3 topIllum    = textureOffset(indirectIllumination, fsTexCoords, ivec2( 0,  1)).rgb;
-//     vec3 botIllum    = textureOffset(indirectIllumination, fsTexCoords, ivec2( 0, -1)).rgb;
-//     vec3 rightIllum  = textureOffset(indirectIllumination, fsTexCoords, ivec2( 1,  0)).rgb;
-//     vec3 leftIllum   = textureOffset(indirectIllumination, fsTexCoords, ivec2(-1,  0)).rgb;
-
-//     // vec3 centerShadow = texture(indirectShadows, fsTexCoords).rgb;
-//     // vec3 topShadow    = textureOffset(indirectShadows, fsTexCoords, ivec2( 0,  1)).rgb;
-//     // vec3 botShadow    = textureOffset(indirectShadows, fsTexCoords, ivec2( 0, -1)).rgb;
-//     // vec3 rightShadow  = textureOffset(indirectShadows, fsTexCoords, ivec2( 1,  0)).rgb;
-//     // vec3 leftShadow   = textureOffset(indirectShadows, fsTexCoords, ivec2(-1,  0)).rgb;
-
-//     vec3 shadowFactor = vec3(0.0);
-//     float numShadowSamples = 0.0;
-//     for (int dx = -dminmax; dx <= dminmax; ++dx) {
-//         for (int dy = -dminmax; dy <= dminmax; ++dy) {
-//             //if (dx != 0 || dy != 0) continue;
-//             ++numShadowSamples;
-//             shadowFactor += textureOffset(indirectShadows, fsTexCoords, ivec2(dx, dy) + ivec2(dx, dy) * multiplier).rgb;
-//         }
-//     }
-//     shadowFactor /= numShadowSamples;
-
-//     //vec3 minColor = tonemap(min(centerIllum, min(topIllum, min(botIllum, min(rightIllum, leftIllum)))));
-//     //vec3 maxColor = tonemap(max(centerIllum, max(topIllum, max(botIllum, max(rightIllum, leftIllum)))));
-
-//     //vec3 gi = (centerIllum + topIllum + botIllum + rightIllum + leftIllum) / 5.0;
-//     vec3 gi = centerIllum;
-//     //gi = gi * shadowFactor;
-//     //vec3 shadow = shadowFactor;
-//     //vec3 shadow = centerShadow;
-//     //gi = gi;
-//     // vec3 gi = centerIllum / (baseColor + PREVENT_DIV_BY_ZERO);
-//     //vec3 gi = centerIllum;
-
-//     // vec3 gi = vec3(0.0);
-//     // float numGiSamples = 0.0;
-//     // for (int dx = -1; dx <= 1; ++dx) {
-//     //     for (int dy = -1; dy <= 1; ++dy) {
-//     //         ++numGiSamples;
-//     //         //vec3 currAlbedo = textureOffset(albedo, fsTexCoords, ivec2(dx, dy) + ivec2(dx, dy) * multiplier).rgb;
-//     //         vec3 illum = textureOffset(indirectIllumination, fsTexCoords, ivec2(dx, dy) + ivec2(dx, dy) * multiplier).rgb;
-//     //         // currAlbedo = vec3(
-//     //         //     max(currAlbedo.r, COMPONENT_WISE_MIN_VALUE), 
-//     //         //     max(currAlbedo.g, COMPONENT_WISE_MIN_VALUE), 
-//     //         //     max(currAlbedo.b, COMPONENT_WISE_MIN_VALUE)
-//     //         // );
-//     //         gi += illum;
-//     //         //gi += vec3(luminance);
-//     //     }
-//     // }
-//     // gi /= numGiSamples;
-
-//     vec3 prevGi = texture(prevIndirectIllumination, prevTexCoords).rgb;
-//     // vec3 tmPrevGi = tonemap(prevGi);
-//     // tmPrevGi = vec3(
-//     //     clamp(tmPrevGi.r, minColor.r, maxColor.r),
-//     //     clamp(tmPrevGi.g, minColor.g, maxColor.g),
-//     //     clamp(tmPrevGi.b, minColor.b, maxColor.b)
-//     // );
-//     // prevGi = inverseTonemap(tmPrevGi);
-
-//     //vec3 illumAvg = centerIllum;
-//     vec3 illumAvg = shadowFactor;
-//     // if (final) {
-//     //     illumAvg = gi * shadowFactor;
-//     // }
-//     //vec3 illumAvg = shadowFactor;
-//     // a is effectively how many frames we can accumulate. So for example 1 / 0.1 = 10,
-//     // 1 / 0.05 = 20, etc.
-//     //vec3 illumAvg = mix(prevGi, shadowFactor, 0.1);
-//     //vec3 illumAvg = mix(prevGi, gi, 1.0 / 1000.0);
-//     //vec3 illumAvg = vec3(difference);
-
-//     combinedColor = screenColor + illumAvg;
-//     giColor = illumAvg;
-//     shadowColor = shadowFactor;
-// }
-
-// void main() {
-//     vec3 screenColor = texture(screen, fsTexCoords).rgb;
-
-//     vec3 centerIllum = texture(indirectIllumination, fsTexCoords).rgb;
-//     //vec3 centerIllum = texture(indirectIllumination, fsTexCoords).rgb;
-//     vec3 topIllum    = textureOffset(indirectIllumination, fsTexCoords, ivec2( 0,  1)).rgb;
-//     vec3 botIllum    = textureOffset(indirectIllumination, fsTexCoords, ivec2( 0, -1)).rgb;
-//     vec3 rightIllum  = textureOffset(indirectIllumination, fsTexCoords, ivec2( 1,  0)).rgb;
-//     vec3 leftIllum   = textureOffset(indirectIllumination, fsTexCoords, ivec2(-1,  0)).rgb;
-    
-//     //vec3 illumAvg = (centerIllum + topIllum + botIllum + rightIllum + leftIllum) / 6.0;
-//     vec3 illumAvg = (centerIllum + topIllum + botIllum + rightIllum + leftIllum) / 5.0;
-
-//     //vec3 illumAvg = texture(indirectIllumination, fsTexCoords).rgb;
-
-//     combinedColor = screenColor + illumAvg;
-//     giColor = illumAvg;
-// }
