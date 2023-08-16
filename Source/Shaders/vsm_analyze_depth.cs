5ee944ceceefb7eff1ea2af688d37bde8c07681c
KTStephano <ktstephano@gmail.com>
2023-08-16 14:42:12 -0600

    Partial fix for some pages not being there when needed

--- Source/Shaders/vsm_analyze_depth.cs
+++ Source/Shaders/vsm_analyze_depth.cs
@@ -25,6 +25,10 @@ layout (std430, binding = 1) buffer block2 {
 shared vec2 depthTextureSize;
 shared ivec2 residencyTableSize;
 
+const int pixelOffsets[] = int[](
+    -1, 0, 1
+);
+
 void main() {
     if (gl_LocalInvocationID == 0) {
         depthTextureSize = textureSize(depthTexture, 0).xy;
@@ -38,6 +42,7 @@ void main() {
 
     // Depth tex coords
     vec2 depthTexCoords = (vec2(xindex, yindex) + vec2(0.5)) / depthTextureSize;
+    //vec2 depthTexCoords = vec2(xindex, yindex) / depthTextureSize;
 
     // Get current depth and convert to world space
     float depth = textureLod(depthTexture, depthTexCoords, 0).r;
@@ -52,15 +57,27 @@ void main() {
     if (cascadeTexCoords.x >= 0 && cascadeTexCoords.x <= 1 &&
         cascadeTexCoords.y >= 0 && cascadeTexCoords.y <= 1) {
 
-        ivec2 pixelCoords = ivec2(cascadeTexCoords * vec2(residencyTableSize - ivec2(1)));
+        for (int x = 0; x < 3; ++x) {
+            int xoffset = pixelOffsets[x];
+            for (int y = 0; y < 3; ++y) {
+                int yoffset = pixelOffsets[y];
+                ivec2 pixelCoords = ivec2(cascadeTexCoords * vec2(residencyTableSize - ivec2(1))) + ivec2(xoffset, yoffset);
 
-        int prev = int(imageLoad(prevFramePageResidencyTable, pixelCoords).r);
-        int current = imageAtomicExchange(currFramePageResidencyTable, pixelCoords, 1);
+                if (pixelCoords.x < 0 || pixelCoords.x >= residencyTableSize.x ||
+                    pixelCoords.y < 0 || pixelCoords.y >= residencyTableSize.y) {
+                    
+                    continue;
+                }
 
-        if (prev == 0 && current == 0) {
-            int original = atomicAdd(numPagesToMakeResident, 1);
-            pageIndices[2 * original] = pixelCoords.x;
-            pageIndices[2 * original + 1] = pixelCoords.y;
+                int prev = int(imageLoad(prevFramePageResidencyTable, pixelCoords).r);
+                int current = imageAtomicExchange(currFramePageResidencyTable, pixelCoords, 1);
+
+                if (prev == 0 && current == 0) {
+                    int original = atomicAdd(numPagesToMakeResident, 1);
+                    pageIndices[2 * original] = pixelCoords.x;
+                    pageIndices[2 * original + 1] = pixelCoords.y;
+                }
+            }
         }
     }
 }
