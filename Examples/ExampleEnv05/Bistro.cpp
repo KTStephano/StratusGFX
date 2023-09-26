d592e3bd9eb6b3a0a3e245c17bb9cb383d54a7d4
KTStephano <ktstephano@gmail.com>
2023-09-26 15:26:29 -0600

    Adjusting spp

--- Examples/ExampleEnv05/Bistro.cpp
+++ Examples/ExampleEnv05/Bistro.cpp
@@ -530,7 +530,7 @@ public:
         // });
 
         auto settings = INSTANCE(RendererFrontend)->GetSettings();
-        settings.skybox = stratus::ResourceManager::Instance()->LoadCubeMap("../Resources/Skyboxes/learnopengl/sbox_", stratus::ColorSpace::NONE, "jpg");
+        settings.skybox = stratus::ResourceManager::Instance()->LoadCubeMap("../Resources/Skyboxes/learnopengl/sbox_", stratus::ColorSpace::SRGB, "jpg");
         settings.cascadeResolution = stratus::RendererCascadeResolution::CASCADE_RESOLUTION_8192;// 16384;
         settings.SetAlphaDepthTestThreshold(0.75f);
         INSTANCE(RendererFrontend)->SetSettings(settings);
